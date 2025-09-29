40d5e7ea412f577a7b823f22dc0a3af3b75db56a
George Boahene <39881594+olibo1zeemoni@users.noreply.github.com>
2025-09-29 05:10:31 +0000

    actor added

---
+++ Actors.playground/Contents.swift
@@ -0,0 +1,65 @@
+import Foundation
+
+var greeting = "Hello, playground"
+print(greeting)
+
+class UnsafeScoreKeeper {
+    var score: Int = 0
+    
+    func increment(){
+        score += 1
+    }
+}
+
+actor SafeScoreKeeper {
+    var score: Int = 0
+    
+    func increment(){
+        score += 1
+    }
+}
+
+
+func runUnsafeRace() async {
+    let scoreKeeper = UnsafeScoreKeeper()
+    
+    await withTaskGroup(of: Void.self) { group in
+        for _ in 0..<1000 {
+            group.addTask {
+                scoreKeeper.increment()
+            }
+        }
+    }
+    
+    print("\(scoreKeeper.score) uns")
+}
+
+Task {
+    await  runUnsafeRace()
+    await  runUnsafeRace()
+    await  runUnsafeRace()
+    await  runUnsafeRace()
+    await  runUnsafeRace()
+}
+
+
+func runSafeRace() async {
+    let scoreKeeper = SafeScoreKeeper()
+    
+    await withTaskGroup(of: Void.self) { group in
+        for _ in 0..<1000 {
+            group.addTask {
+                await scoreKeeper.increment()
+            }
+        }
+    }
+    print(await scoreKeeper.score)
+}
+
+Task {
+    await runSafeRace()
+    await runSafeRace()
+    await runSafeRace()
+    await runSafeRace()
+    await runSafeRace()
+}
